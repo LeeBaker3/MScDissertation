9579bcf38de759b075290df35ccdf659b55b3100
Lee Baker <lee.baker1@ymail.com>
2014-07-13 22:04:46 +1000

    Added JSF Pages

---
+++ AtomicInformationConfigurationManager/src/main/java/com/mycompany/atomicinformationconfigurationmanager/entities/ArtefactdistributionFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.atomicinformationconfigurationmanager.entities;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Lee Baker
+ */
+@Stateless
+public class ArtefactdistributionFacade extends AbstractFacade<Artefactdistribution> {
+    @PersistenceContext(unitName = "com.mycompany_AtomicInformationConfigurationManager_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ArtefactdistributionFacade() {
+        super(Artefactdistribution.class);
+    }
+    
+}
