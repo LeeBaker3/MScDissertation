0adb6345b5af8bec4d81c5c6eb77ae48a7a921cc
Lee Baker <lee.baker1@ymail.com>
2014-07-25 23:21:38 +1000

    Completed test Data

--- AtomicInformationConfigurationManager/SQLScripts/MySQLInsertTestData.sql
+++ AtomicInformationConfigurationManager/SQLScripts/MySQLInsertTestData.sql
@@ -5,7 +5,7 @@
 -- Descritpion: Relational Database For Liverpool University MSc Dissertation 
 -- Insert test Data into Database
 
-USE `AtomicInformationConfigurationManagerDB` ;
+USE `AtomicInformationConfigurationManagerDB`;
 
 -- -----------------------------------------------------
 -- Insert Data into Project Table
@@ -43,7 +43,7 @@ INSERT INTO project (ProjectReference, ProjectName, VersionNumber, IsCurrentVers
 -- -----------------------------------------------------
 
 INSERT INTO artefact (`VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `ArtefactName`, `ArtefactMajorVersionNumber`, `ArtefactMinorVersionNumber`, `ProjectID`) 
-	VALUES (1, true, '', true, 'Test Artefact 1', '1', '0', 1)  ON DUPLICATE KEY UPDATE
+	VALUES (1, true, '', true, 'Test Artefact 1 Requirements Specification', '1', '0', 1)  ON DUPLICATE KEY UPDATE
 	VersionNumber=VALUES(VersionNumber), 
 	IsCurrentVersion=VALUES(IsCurrentVersion), 
 	PreviousVersionReference=VALUES(PreviousVersionReference), 
@@ -54,7 +54,7 @@ INSERT INTO artefact (`VersionNumber`, `IsCurrentVersion`, `PreviousVersionRefer
 	ProjectID=VALUES(ProjectID);
 
 INSERT INTO artefact (`VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `ArtefactName`, `ArtefactMajorVersionNumber`, `ArtefactMinorVersionNumber`, `ProjectID`) 
-	VALUES (1, true, '', true, 'Test Artefact 2', '1', '0', 1)  ON DUPLICATE KEY UPDATE
+	VALUES (1, true, '', true, 'Test Artefact 2 Functional Specificaltion', '1', '0', 1)  ON DUPLICATE KEY UPDATE
 	VersionNumber=VALUES(VersionNumber), 
 	IsCurrentVersion=VALUES(IsCurrentVersion), 
 	PreviousVersionReference=VALUES(PreviousVersionReference), 
@@ -65,7 +65,7 @@ INSERT INTO artefact (`VersionNumber`, `IsCurrentVersion`, `PreviousVersionRefer
 	ProjectID=VALUES(ProjectID);
 
 INSERT INTO artefact (`VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `ArtefactName`, `ArtefactMajorVersionNumber`, `ArtefactMinorVersionNumber`, `ProjectID`) 
-	VALUES (1, true, '', true, 'Test Artefact 3', '1', '0', 2)  ON DUPLICATE KEY UPDATE
+	VALUES (1, true, '', true, 'Test Artefact 3 Project Initiation Document', '1', '0', 2)  ON DUPLICATE KEY UPDATE
 	VersionNumber=VALUES(VersionNumber), 
 	IsCurrentVersion=VALUES(IsCurrentVersion), 
 	PreviousVersionReference=VALUES(PreviousVersionReference), 
@@ -79,22 +79,126 @@ INSERT INTO artefact (`VersionNumber`, `IsCurrentVersion`, `PreviousVersionRefer
 -- Insert Data into atomicinformation Table
 -- -----------------------------------------------------
 
--- TO DO
+ INSERT INTO atomicinformation (`ArtefactID`, `TypeOfAtomicInformationID`, `VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `Content`, `ProjectID`) 
+	VALUES (1, 1, 1, true, NULL, true, 'Requirement 1: Must Be able to Save Something', 1) ON DUPLICATE KEY UPDATE
+	ArtefactID=VALUES(ArtefactID),
+	TypeOfAtomicInformationID=VALUES(TypeOfAtomicInformationID),
+	VersionNumber=VALUES(VersionNumber), 
+	IsCurrentVersion=VALUES(IsCurrentVersion), 
+	PreviousVersionReference=VALUES(PreviousVersionReference), 
+	EntityActive=VALUES(EntityActive), 
+	Content=VALUES(Content),
+	ProjectID=VALUES(ProjectID);
+
+ INSERT INTO atomicinformation (`ArtefactID`, `TypeOfAtomicInformationID`, `VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `Content`, `ProjectID`) 
+	VALUES (1, 3, 1, true, NULL, true, 'Senior User: Joe Bloggs', 1) ON DUPLICATE KEY UPDATE
+	ArtefactID=VALUES(ArtefactID),
+	TypeOfAtomicInformationID=VALUES(TypeOfAtomicInformationID),
+	VersionNumber=VALUES(VersionNumber), 
+	IsCurrentVersion=VALUES(IsCurrentVersion), 
+	PreviousVersionReference=VALUES(PreviousVersionReference), 
+	EntityActive=VALUES(EntityActive), 
+	Content=VALUES(Content),
+	ProjectID=VALUES(ProjectID);
+
+ INSERT INTO atomicinformation (`ArtefactID`, `TypeOfAtomicInformationID`, `VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `Content`, `ProjectID`) 
+	VALUES (2, 2, 1, true, NULL, true, 'Function001: When User Presses Save Check No Current Matching Records', 1) ON DUPLICATE KEY UPDATE
+	ArtefactID=VALUES(ArtefactID),
+	TypeOfAtomicInformationID=VALUES(TypeOfAtomicInformationID),
+	VersionNumber=VALUES(VersionNumber), 
+	IsCurrentVersion=VALUES(IsCurrentVersion), 
+	PreviousVersionReference=VALUES(PreviousVersionReference), 
+	EntityActive=VALUES(EntityActive), 
+	Content=VALUES(Content),
+	ProjectID=VALUES(ProjectID);
+
+ INSERT INTO atomicinformation (`ArtefactID`, `TypeOfAtomicInformationID`, `VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `Content`, `ProjectID`) 
+	VALUES (3, 4, 1, true, NULL, true, 'John Doe', 1) ON DUPLICATE KEY UPDATE
+	ArtefactID=VALUES(ArtefactID),
+	TypeOfAtomicInformationID=VALUES(TypeOfAtomicInformationID),
+	VersionNumber=VALUES(VersionNumber), 
+	IsCurrentVersion=VALUES(IsCurrentVersion), 
+	PreviousVersionReference=VALUES(PreviousVersionReference), 
+	EntityActive=VALUES(EntityActive), 
+	Content=VALUES(Content),
+	ProjectID=VALUES(ProjectID);
 
 -- -----------------------------------------------------
--- Insert Data into Project artefactdistribution Table
+-- Insert Data into distrbutionrecipient  Table
 -- -----------------------------------------------------
+INSERT INTO distributionrecipient (`ProjectID`, `VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `FirstName`, `Surname`, `EMailAddress`, `MobileNumber`, `OfficeNumber`, `CompanyName`) 
+	VALUES (1, 1, true, '', true, 'John', 'Doe', 'john.doe@somecompany.com', '123456789', '987654321', 'Test Company 1') ON DUPLICATE KEY UPDATE
+	ProjectID=VALUES(ProjectID),
+	VersionNumber=VALUES(VersionNumber), 
+	IsCurrentVersion=VALUES(IsCurrentVersion), 
+	PreviousVersionReference=VALUES(PreviousVersionReference), 
+	EntityActive=VALUES(EntityActive), 
+	FirstName=VALUES(FirstName),
+	Surname=VALUES(Surname),
+	EMailAddress=VALUES(EMailAddress),
+	MobileNumber=VALUES(MobileNumber),
+	OfficeNumber=VALUES(OfficeNumber),
+	CompanyName=VALUES(CompanyName);
 
--- TO DO
+INSERT INTO distributionrecipient (`ProjectID`, `VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `FirstName`, `Surname`, `EMailAddress`, `MobileNumber`, `OfficeNumber`, `CompanyName`) 
+	VALUES (1, 1, true, '', true, 'Joe', 'Bloggs', 'joe.bloggs@somecompany.com', '123456789', '987654321', 'Test Company 2') ON DUPLICATE KEY UPDATE
+	ProjectID=VALUES(ProjectID),
+	VersionNumber=VALUES(VersionNumber), 
+	IsCurrentVersion=VALUES(IsCurrentVersion), 
+	PreviousVersionReference=VALUES(PreviousVersionReference), 
+	EntityActive=VALUES(EntityActive), 
+	FirstName=VALUES(FirstName),
+	Surname=VALUES(Surname),
+	EMailAddress=VALUES(EMailAddress),
+	MobileNumber=VALUES(MobileNumber),
+	OfficeNumber=VALUES(OfficeNumber),
+	CompanyName=VALUES(CompanyName);
 
+INSERT INTO distributionrecipient (`ProjectID`, `VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `FirstName`, `Surname`, `EMailAddress`, `MobileNumber`, `OfficeNumber`, `CompanyName`) 
+	VALUES (1, 1, true, '', true, 'Jane', 'Roe', 'jane.roe@somecompany.com', '123456789', '987654321', 'Test Company 1') ON DUPLICATE KEY UPDATE
+	ProjectID=VALUES(ProjectID),
+	VersionNumber=VALUES(VersionNumber), 
+	IsCurrentVersion=VALUES(IsCurrentVersion), 
+	PreviousVersionReference=VALUES(PreviousVersionReference), 
+	EntityActive=VALUES(EntityActive), 
+	FirstName=VALUES(FirstName),
+	Surname=VALUES(Surname),
+	EMailAddress=VALUES(EMailAddress),
+	MobileNumber=VALUES(MobileNumber),
+	OfficeNumber=VALUES(OfficeNumber),
+	CompanyName=VALUES(CompanyName);
 -- -----------------------------------------------------
--- Insert Data into Project methodofdistribution Table
+-- Insert Data into artefactdistribution Table
 -- -----------------------------------------------------
+INSERT INTO artefactdistribution (`ArtefactID`, `MethodOfDistributionID`, `VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `DateOfArtefactDistribution`, `DistributionRecipientID`) 
+	VALUES (2, 1, 1, true, '', true, '2014-07-25', 1) ON DUPLICATE KEY UPDATE
+	ArtefactID=VALUES(ArtefactID),
+	MethodOfDistributionID=VALUES(MethodOfDistributionID),
+	VersionNumber=VALUES(VersionNumber), 
+	IsCurrentVersion=VALUES(IsCurrentVersion), 
+	PreviousVersionReference=VALUES(PreviousVersionReference), 
+	EntityActive=VALUES(EntityActive),
+	DateOfArtefactDistribution=VALUES(DateOfArtefactDistribution),
+	DistributionRecipientID=VALUES(DistributionRecipientID);
 
--- TO DO
+INSERT INTO artefactdistribution (`ArtefactID`, `MethodOfDistributionID`, `VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `DateOfArtefactDistribution`, `DistributionRecipientID`) 
+	VALUES (3, 2, 1, true, '', true, '2014-07-12', 2) ON DUPLICATE KEY UPDATE
+	ArtefactID=VALUES(ArtefactID),
+	MethodOfDistributionID=VALUES(MethodOfDistributionID),
+	VersionNumber=VALUES(VersionNumber), 
+	IsCurrentVersion=VALUES(IsCurrentVersion), 
+	PreviousVersionReference=VALUES(PreviousVersionReference), 
+	EntityActive=VALUES(EntityActive),
+	DateOfArtefactDistribution=VALUES(DateOfArtefactDistribution),
+	DistributionRecipientID=VALUES(DistributionRecipientID);
 
--- -----------------------------------------------------
--- Insert Data into Project typeofatomicinformation Table
--- -----------------------------------------------------
-
--- TO DO
+INSERT INTO artefactdistribution (`ArtefactID`, `MethodOfDistributionID`, `VersionNumber`, `IsCurrentVersion`, `PreviousVersionReference`, `EntityActive`, `DateOfArtefactDistribution`, `DistributionRecipientID`) 
+	VALUES (2, 1, 1, true, '', true, '2014-07-14', 3) ON DUPLICATE KEY UPDATE
+	ArtefactID=VALUES(ArtefactID),
+	MethodOfDistributionID=VALUES(MethodOfDistributionID),
+	VersionNumber=VALUES(VersionNumber), 
+	IsCurrentVersion=VALUES(IsCurrentVersion), 
+	PreviousVersionReference=VALUES(PreviousVersionReference), 
+	EntityActive=VALUES(EntityActive),
+	DateOfArtefactDistribution=VALUES(DateOfArtefactDistribution),
+	DistributionRecipientID=VALUES(DistributionRecipientID);
