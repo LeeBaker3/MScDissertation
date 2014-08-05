a6794110f83352f55df44c1b837188edc692c77c
Lee Baker <lee.baker1@ymail.com>
2014-08-05 23:14:27 +1000

    Added Hash Code

--- AtomicInformationConfigurationManager/src/main/java/com/mycompany/atomicinformationconfigurationmanager/stateful/SelectedProject.java
+++ AtomicInformationConfigurationManager/src/main/java/com/mycompany/atomicinformationconfigurationmanager/stateful/SelectedProject.java
@@ -9,6 +9,7 @@ package com.mycompany.atomicinformationconfigurationmanager.stateful;
 import com.mycompany.atomicinformationconfigurationmanager.entities.ArtefactController;
 import com.mycompany.atomicinformationconfigurationmanager.entities.Project;
 import java.io.Serializable;
+import java.util.Objects;
 import javax.ejb.Stateful;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -17,10 +18,7 @@ import javax.inject.Named;
  *
  * @author Lee Baker
  * 
- * This class is used to hold the currently selected project by the user
-<<<<<<< HEAD
- * the variable project holds the reference to the Project Entity that has 
- * been selected.
+ * This class is used to hold a reference to currently selected project by the user
  */
 @Named("selectedProject")
 @Stateful
@@ -36,5 +34,28 @@ public class SelectedProject implements Serializable {
     public void setProject(Project project) {
         this.project = project;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.project);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SelectedProject other = (SelectedProject) obj;
+        if (!Objects.equals(this.project, other.project)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
