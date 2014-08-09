6562e1d57493e2c9f90dff04f63c389f13f4cd07
Lee Baker <lee.baker1@ymail.com>
2014-08-09 15:08:02 +1000

    Renamed from Facade
    Renamed Facade class to Save Retrieve

---
+++ AtomicInformationConfigurationManager/src/main/java/com/mycompany/atomicinformationconfigurationmanager/entities/ArtefactSaveRetrieve.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.atomicinformationconfigurationmanager.entities;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author Lee Baker
+ */
+@Stateless
+public class ArtefactSaveRetrieve extends BaseSaveRetrieveAbstract<Artefact> {
+    @PersistenceContext(unitName = "AtomicInformationConfigManagerPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ArtefactSaveRetrieve() {
+        super(Artefact.class);
+    }
+    
+    public List<Artefact> findByEntityActiveAndProjectID (Project project, boolean entityActive){
+        TypedQuery<Artefact> query = em.createNamedQuery("Artefact.findByEntityActiveAndProjectID", Artefact.class);
+        query.setParameter("projectID", project);
+        query.setParameter("entityActive", entityActive);
+        List<Artefact> results = query.getResultList();
+        return results;
+    }
+    
+    public List<Artefact> findRangeEntityActiveAndProjectID (int[] range, Boolean entityActive, Project project){
+        TypedQuery<Artefact> query = em.createNamedQuery("Artefact.findByEntityActiveAndProjectID", Artefact.class);
+        query.setParameter("projectID", project);
+        query.setParameter("entityActive", entityActive);
+        query.setMaxResults(range[1] - range[0] + 1);
+        query.setFirstResult(range[0]);
+        List<Artefact> results = query.getResultList();
+        return results;
+    }
+    
+        public int countEntityActiveAndProjectID(Project project, Boolean entityActive){
+        TypedQuery<Artefact> query = em.createNamedQuery("Artefact.findByEntityActiveAndProjectID", Artefact.class);
+        query.setParameter("projectID", project);
+        query.setParameter("entityActive", entityActive);
+        List<Artefact> results = query.getResultList();
+        return results.size();
+    }
+}
---
+++ AtomicInformationConfigurationManager/src/main/java/com/mycompany/atomicinformationconfigurationmanager/entities/ProjectSaveRetrieve.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.atomicinformationconfigurationmanager.entities;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Lee Baker
+ */
+@Stateless
+public class ProjectSaveRetrieve extends BaseSaveRetrieveAbstract<Project> {
+    @PersistenceContext(unitName = "AtomicInformationConfigManagerPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ProjectSaveRetrieve() {
+        super(Project.class);
+    }
+    
+}
