ec30e5a65a5662cafc0b031d62db0433065483b9
Lee Baker <lee.baker1@ymail.com>
2014-09-03 13:18:38 +1000

    Working changes 03/09/14

---
+++ AtomicInformationConfigurationManager/src/main/java/com/mycompany/atomicinformationconfigurationmanager/businessrules/ReturnToArtefactView.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.atomicinformationconfigurationmanager.businessrules;
+
+import com.mycompany.atomicinformationconfigurationmanager.entities.Artefact.Artefact;
+import com.mycompany.atomicinformationconfigurationmanager.entities.Artefactatomicinformation.ArtefactatomicinformationController;
+import com.mycompany.atomicinformationconfigurationmanager.entities.artefactdistribution.ArtefactdistributionController;
+import java.util.Objects;
+import javax.ejb.Stateless;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Lee Baker
+ * 
+ * This class is used to hold a reference to the currently selected artefact by the user
+ */
+@Named("returnToArtefactView")
+@Stateless
+public class ReturnToArtefactView {
+    
+    @Inject
+    private ArtefactatomicinformationController artefactatomicinformationController;
+    @Inject
+    private ArtefactdistributionController artefactdistributionController;
+    
+    public String returnToView(){
+        artefactatomicinformationController.recreateModel();
+        artefactdistributionController.recreateModel();
+        return "/Faces/artefact/View";
+    }
+}
